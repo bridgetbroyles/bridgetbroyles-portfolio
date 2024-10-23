e72cd7b8c3d543559817a00f1fbd842b2196a948
Bridget Broyles <bridgetcb2@gmail.com>
2024-10-22 21:52:10 -0500

    Add files via upload

---
+++ freshmanCode/largerDigits.java
@@ -0,0 +1,27 @@
+public class largerDigits {
+
+	public static void main(String[] args) {
+		System.out.print(largerDigits(8921, 97325));
+	}
+
+	public static int largerDigits(int a, int b) {
+		int c = 0;
+		int placeValue = 1;
+
+		while (a > 0 || b > 0) {
+			int digitA = a % 10;
+			int digitB = b % 10;
+
+			int largerDigit = Math.max(digitA, digitB);
+
+			c += largerDigit * placeValue;
+
+			placeValue *= 10;
+
+			a /= 10;
+			b /= 10;
+		}
+
+		return c;
+	}
+}
