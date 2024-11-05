cc7ae98d02002570da5141e417be189f6b6303d2
Bridget Broyles <bridgetcb2@gmail.com>
2024-11-05 01:02:59 -0600

    Update BJP5 Exercise 7.5: mode.java

--- favorite-practiceIts/BJP5 Exercise 7.5: mode.java
+++ favorite-practiceIts/BJP5 Exercise 7.5: mode.java
@@ -6,3 +6,22 @@ Assume that the array has at least one element and that every element in the arr
 For example, if the array passed contains the values {27, 15, 15, 11, 27}, your method should return 15. 
 (Hint: You may wish to look at the Tally program from earlier in this chapter to get an idea of how to solve this problem.)
 */
+public static int mode(int[] array) {
+    int[] counterArray = new int[101];
+  
+    for (int i = 0; i < array.length; i++) {
+        counterArray[array[i]]++;
+    }
+
+    int maxCount = 0;
+    int mode = 0;
+
+    for (int i = 0; i < counterArray.length; i++) {
+        if (counterArray[i] > maxCount) {
+            maxCount = counterArray[i];
+            mode = i;
+        }
+    }
+
+    return mode;
+}
