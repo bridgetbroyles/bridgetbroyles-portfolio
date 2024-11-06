0789fe9a00cf6d84ad323e429045dc4cf57fc3c2
Bridget Broyles <bridgetcb2@gmail.com>
2024-11-06 10:38:39 -0600

    Update PrintWithoutHtml.java

--- freshmanCode/PrintWithoutHtml.java
+++ freshmanCode/PrintWithoutHtml.java
@@ -14,7 +14,7 @@ public class PrintWithoutHtml {
 		System.out.println("2nd way: ");
 		while (input.hasNextLine()) {
 			String line = input.nextLine();
-			while (line.contains("<") || line.contains(">")) {
+			while (line.contains("<") && line.contains(">")) {
 				int index1 = line.indexOf("<");
 				int index2 = line.indexOf(">");
 				line = line.substring(0, index1) + line.substring(index2 + 1);
