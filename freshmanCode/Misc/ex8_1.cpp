b459b7c017bab3a9e74f32ce93109dcc22e6a0e6
Bridget Broyles <bridgetcb2@gmail.com>
2025-06-19 09:18:53 -0500

    Update ex8_1.cpp

--- freshmanCode/Misc/ex8_1.cpp
+++ freshmanCode/Misc/ex8_1.cpp
@@ -10,6 +10,7 @@ public:
   MotorMenu() {
     _lastButton = 0;
     _whichMotor = 0;
+    _leftSpeed = _rightSpeed = 0;
   }
 
   void buttonPress(byte button) {
@@ -17,34 +18,22 @@ public:
       case 0:
         break;
       case 1: 
-        if(_lastButton != button) {
+        if (_lastButton != button) {
           _whichMotor = !_whichMotor;
         }
         break;
       case 2:
-        if(_whichMotor) {
-          _rightSpeed++;
-          if(_rightSpeed > 100) {
-            _rightSpeed = 100;
-          }
+        if (_whichMotor) {
+          _rightSpeed = min(_rightSpeed + 1, 100);
         } else {
-          _leftSpeed++;
-          if(_leftSpeed > 100) {
-            _leftSpeed = 100;
-          }
+          _leftSpeed  = min(_leftSpeed  + 1, 100);
         }
         break;
       case 3:
-        if(_whichMotor) {
-          _rightSpeed--;
-          if(_rightSpeed < -100) {
-            _rightSpeed = -100;
-          }
+        if (_whichMotor) {
+          _rightSpeed = max(_rightSpeed - 1, -100);
         } else {
-          _leftSpeed++;
-          if(_leftSpeed < 100) {
-            _leftSpeed = -100;
-          }
+          _leftSpeed  = max(_leftSpeed  - 1, -100);
         }
         break;
     }
@@ -58,31 +47,59 @@ public:
   void printMenu() {
     String lineOne = String(_leftSpeed) + "  " + String(_rightSpeed);
     one.lcd1(lineOne);
-    if(_whichMotor) {
-      one.lcd2("--->");
-    } else {
-      one.lcd2("<---");
-    }
+    one.lcd2(_whichMotor ? "--->" : "<---");
   }
 
 protected:
-  byte _lastButton;
-  bool _whichMotor;
-  int _leftSpeed, _rightSpeed;
+  byte  _lastButton;
+  bool  _whichMotor;
+  int   _leftSpeed, _rightSpeed;
 };
 
 MotorMenu menu;
 
-int counter = 0;
+class TurnAndPush {
+public:
+  TurnAndPush(int thresh) : threshold(thresh) {}
+
+  void reactToObstacles() {
+    int leftO  = one.readLeftRangeSensor();
+    int rightO = one.readRightRangeSensor();
+    byte obstacleDetection;
+    if (leftO  <= threshold && rightO <= threshold) {
+      obstacleDetection = 0;
+    }
+    else if (leftO  > threshold && rightO <= threshold) {
+      obstacleDetection = 1;
+    }
+    else if (leftO  <= threshold && rightO > threshold) {
+      obstacleDetection = 2;
+    }
+    else {
+      obstacleDetection = 3;
+    }
+    switch (obstacleDetection) {
+      case 0: one.lcd1("No obstacle"); break;
+      case 1: one.lcd1("Left");        break;
+      case 2: one.lcd1("Right");       break;
+      case 3: one.lcd1("Both");        break;
+    }
+  }
+
+private:
+  int threshold;
+};
+
+TurnAndPush tp(300);
 
 void setup() {
   Serial.begin(115200);
   one.spiConnect(SSPIN);
   one.stop();
+  one.obstacleSensorsEmitters(true);
 }
 
 void loop() {
-  menu.buttonPress(one.readButton());
-  menu.runMotors();
-  menu.printMenu();
+  tp.reactToObstacles();
+  delay(100);
 }
